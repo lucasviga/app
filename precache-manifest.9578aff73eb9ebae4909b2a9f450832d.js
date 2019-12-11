4db5ea9dc878d809f1bec5b92cbf2c7ee4e8a110
Lucas Viga <lucasviga12@gmail.com>
2019-12-11 14:14:57 -0400

    Updates

---
+++ precache-manifest.9578aff73eb9ebae4909b2a9f450832d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f028203f92ddb528c0c4054d9570fc45",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "cca93b5870263e9e5709",
+    "url": "/app/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "223598b6c05905b2b924",
+    "url": "/app/static/js/2.24cb694b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app/static/js/2.24cb694b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cca93b5870263e9e5709",
+    "url": "/app/static/js/main.ef8f2be2.chunk.js"
+  },
+  {
+    "revision": "400952b4323f6d47b1b6",
+    "url": "/app/static/js/runtime-main.033a61e0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
